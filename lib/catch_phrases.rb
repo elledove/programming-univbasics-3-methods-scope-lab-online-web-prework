e5cc2594283b21af0eea7b22d2672e5b5b8651d6
Lauren Dove <elledoveinc@gmail.com>
2019-08-10 02:33:52 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,15 +6,15 @@ mario
 def toadstool
   puts status = "Thank You Mario! But Our Princess Is In Another Castle!"
 end
-toadstool
+
 
 
 def link
   puts "It's Dangerous To Go Alone! Take This"
 end
-link
+
 
 def any_phrase(phrase)
   puts "Do A Barrel Roll!"
   end
-  any_prhase
+  
